ff6f83462c84894746c2a053cbf1c37503765229
CoderLivingDeath <CoderLivingDeath@gmail.com>
2024-11-07 00:13:26 +0700

    Реализован интерфейс сериализации и сохранения мета данных через конкретные реализации

---
+++ Assets/Scripts/Serialization/SaveLoader.cs
@@ -0,0 +1,35 @@
+﻿using Assets.Scripts.Meta.Serialization;
+using Assets.Scripts.Serialization.Meta;
+
+namespace Assets.Scripts.Serialization
+{
+#nullable enable
+    public class SaveLoader
+    {
+        private PlayerMeta? _playerMeta_m = null;
+
+        private ISerialization _serialization;
+
+        public SaveLoader(ISerialization serialization)
+        {
+            _serialization = serialization;
+        }
+
+        public PlayerMeta GetPlayerMeta()
+        {
+            if (_playerMeta_m != null) return _playerMeta_m;
+
+            var meta = _serialization.Load<PlayerMeta>(MetaKeys.PlayerMeta);
+            _playerMeta_m = meta;
+
+            return meta;
+        }
+
+        public void SavePlayerMeta(PlayerMeta playerMeta)
+        {
+            _serialization.Save(MetaKeys.PlayerMeta, playerMeta);
+            _playerMeta_m = playerMeta;
+        }
+    }
+}
+#nullable disable
