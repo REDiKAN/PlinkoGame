a07fb7d30f1585f0632442a3b954543729c1760a
CoderLivingDeath <CoderLivingDeath@gmail.com>
2024-11-08 17:43:35 +0700

    GameCycleController: исправлен нейминг, добавлены методы валидации и проверки связанные с Coins. Добавлены метады обработки победы и проигрыша(NotImplemented) и проверка состояния игры.

--- Assets/Scripts/GameCycleController.cs
+++ Assets/Scripts/GameCycleController.cs
@@ -3,8 +3,14 @@ using UnityEngine;
 
 public class GameCycleController : MonoBehaviour
 {
+
     public int Balls;
     public int Coins;
+
+    public bool CurrentBallInGame = false;
+
+
+    public event Action<int> OnPlayerRewardWithCoins;
 
     public void Init(int balls)
     {
@@ -15,14 +21,79 @@ public class GameCycleController : MonoBehaviour
     {
         DontDestroyOnLoad(gameObject);
     }
-
-    public void OnReward(int reward)
+    public void UpdateGameState()
     {
-        Coins += reward;
+        if (Balls == 0 && !CurrentBallInGame)
+        {
+            OnLooseHandler();
+            return;
+        }
+        throw new NotImplementedException();
+    }
+    public void OnLooseHandler()
+    {
+        Debug.Log("Player is looser.");
+        throw new NotImplementedException();
     }
 
-    public void OnLoose()
+    public void OnWinHadler()
     {
         throw new NotImplementedException();
+    }
+    public void RewardPlayeWithCoins(int coins)
+    {
+        if (TryAddCoins(coins))
+        {
+            OnPlayerRewardWithCoins?.Invoke(coins);
+        }
+    }
+
+    public void TakeCoinsFromPlayer(int coins)
+    {
+        if (CheckPossibleSubtractFromCoins(coins))
+        {
+            int newCoins = Coins - coins;
+            SetCoins(newCoins);
+            return;
+        }
+        throw new NotImplementedException();
+    }
+
+    public void SetCoins(int coins)
+    {
+        if (CheckValidCoins(coins))
+        {
+            Coins = coins;
+            return;
+        }
+
+        throw new Exception($"Value {nameof(coins)}: [{coins}] - is invalid.");
+    }
+
+    public bool TryAddCoins(int coins)
+    {
+        if (!CheckValidCoins(coins)) return false;
+
+        int newCoins = Coins + coins;
+        SetCoins(newCoins);
+
+        return true;
+    }
+
+    public bool CheckBallsIsEmpty()
+    {
+        return Balls == 0;
+    }
+
+    public bool CheckPossibleSubtractFromCoins(int coins)
+    {
+        return Coins >= coins;
+    }
+
+    public bool CheckValidCoins(int coins)
+    {
+        if (coins < 0) return false;
+
+        return true;
     }
 }
