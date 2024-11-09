0bcfc48159421a6303687d1b4cdd93f6ac2e5f28
Cre1tor <Cre1tor@outlook.com>
2024-11-09 16:35:09 +0300

    На Р стрелять

--- Assets/ShootCannon.cs
+++ Assets/ShootCannon.cs
@@ -8,11 +8,12 @@ public class ShootCannon : MonoBehaviour
 {
     [SerializeField] private GameObject Bullet;
     [SerializeField] private GameObject Crosshair;
+    private bool CanShoot = true;
 
     private void Start()
     {
         Observable.EveryGameObjectUpdate()
-            .Where(_ => Input.GetKeyDown(KeyCode.P))
+            .Where(_ => Input.GetKeyDown(KeyCode.P) && CanShoot)
             .Subscribe(_ => { 
                 GameObject gmObject = Instantiate(Bullet);
                 gmObject.transform.position = Crosshair.transform.position;
@@ -20,7 +21,11 @@ public class ShootCannon : MonoBehaviour
                 gmObject.GetComponent<Rigidbody2D>().AddForce(dir*10, ForceMode2D.Impulse);
                 var rot = Mathf.Atan2(dir.y, dir.x);
                 gmObject.transform.rotation = Quaternion.EulerRotation(new Vector3(0, 0, rot * Mathf.Deg2Rad));
-
+                CanShoot = false;
+                Observable.Timer(System.TimeSpan.FromSeconds(1)).Subscribe(_ =>
+                {
+                    CanShoot = true;
+                });
             });
     }
 }
