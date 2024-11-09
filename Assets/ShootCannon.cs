838bc6b02c1b7260a3671c09eea779380fe251fd
Cre1tor <Cre1tor@outlook.com>
2024-11-09 16:18:46 +0300

    Добавил стреляющую пушку на букву K

---
+++ Assets/ShootCannon.cs
@@ -0,0 +1,26 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UniRx;
+using UnityEngine;
+
+public class ShootCannon : MonoBehaviour
+{
+    [SerializeField] private GameObject Bullet;
+    [SerializeField] private GameObject Crosshair;
+
+    private void Start()
+    {
+        Observable.EveryGameObjectUpdate()
+            .Where(_ => Input.GetKeyDown(KeyCode.P))
+            .Subscribe(_ => { 
+                GameObject gmObject = Instantiate(Bullet);
+                gmObject.transform.position = Crosshair.transform.position;
+                Vector2 dir = (Crosshair.transform.position - transform.position).normalized;
+                gmObject.GetComponent<Rigidbody2D>().AddForce(dir*10, ForceMode2D.Impulse);
+                var rot = Mathf.Atan2(dir.y, dir.x);
+                gmObject.transform.rotation = Quaternion.EulerRotation(new Vector3(0, 0, rot * Mathf.Deg2Rad));
+
+            });
+    }
+}
