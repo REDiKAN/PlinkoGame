8afca5a2868b3d8d607604cbeae15ae5980129a4
Cre1tor <Cre1tor@outlook.com>
2024-11-16 18:51:48 +0300

    AddInterface

--- Assets/ShootCannon.cs
+++ Assets/ShootCannon.cs
@@ -4,6 +4,7 @@ using System.Collections.Generic;
 using UniRx;
 using UnityEngine;
 
+
 public class ShootCannon : MonoBehaviour
 {
     [SerializeField] private GameObject Bullet;
@@ -16,15 +17,17 @@ public class ShootCannon : MonoBehaviour
     private void Start()
     {
         Observable.EveryGameObjectUpdate()
-            .Where(_ => Input.GetKeyDown(KeyCode.P) && CanShoot)
-            .Subscribe(_ => { 
-                GameObject gmObject = Instantiate(Bullet);
-                gmObject.transform.position = Crosshair.transform.position;
-                Vector2 dir = (Crosshair.transform.position - transform.position).normalized;
+            .Where(_ => Input.GetKeyDown(KeyCode.P) && CanShoot && !_controller.CheckBallsIsEmpty())
+            .Subscribe(_ => {
+                GameObject gmObject = Instantiate(_bullet);
+                gmObject.transform.position = _crosshair.transform.position;
+                Vector2 dir = (_crosshair.transform.position - transform.position).normalized;
                 gmObject.GetComponent<Rigidbody2D>().AddForce(dir*10, ForceMode2D.Impulse);
                 var rot = Mathf.Atan2(dir.y, dir.x);
                 gmObject.transform.rotation = Quaternion.EulerRotation(new Vector3(0, 0, rot * Mathf.Deg2Rad));
                 CanShoot = false;
+                _controller.Balls--;
+                
                 Observable.Timer(System.TimeSpan.FromSeconds(1)).Subscribe(_ =>
                 {
                     CanShoot = true;
